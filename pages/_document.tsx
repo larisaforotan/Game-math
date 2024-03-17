0ae24a4917849968443c9194a92ef84fbe1e5bf4
larisaforotan <forotanlarisa@gmail.com>
2024-03-17 21:51:09 +0330

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,10 +12,7 @@ class MyDocument extends Document {
           />
 
 
-          <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png" />
-          <link rel="manifest" href="favicons/site.webmanifest" />
+      
 
         </Head>
         <body className="bg-fixed bg-gradient-to-r from-green to-blue-500 dark:from-dark-500 dark:to-dark-700 dark:text-white">
