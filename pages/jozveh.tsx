0bd9596e6448e265c99b9933183bad75240b5437
larisaforotan <forotanlarisa@gmail.com>
2024-03-18 18:03:08 +0330

    Create jozveh.tsx

---
+++ pages/jozveh.tsx
@@ -0,0 +1,27 @@
+import {
+	NextPage,
+  } from "next";
+
+  
+  
+  
+  const Jozveh: NextPage = () => {
+  
+  
+  
+  
+  
+  
+	return (
+	<div className="flex flex-col gap-8">
+		<img src="images/1.jpg" className="w-full h-full" />
+		<img src="images/2.jpg" className="w-full h-full" />
+		<img src="images/3.jpg" className="w-full h-full" />
+	</div>
+	);
+  };
+  
+
+  
+  export default Jozveh;
+  
